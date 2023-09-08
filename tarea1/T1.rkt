87517336efa6b7381327e097131179cd209a6824
Pablo <112183476+pabloscopeo@users.noreply.github.com>
2023-09-08 03:19:22 -0300

    finished p1 + p2.a

--- tarea1/T1.rkt
+++ tarea1/T1.rkt
@@ -30,6 +30,23 @@
 (test (orp? (varp "a")) #f)
 (test (orp? (orp (varp "a") (varp "b"))) #t)
 
+;; print-prop :: Prop -> String
+;; imprime una proposición en forma de string, para que sea más fácil de leer/debuggear
+(define (print-prop prop)
+    (match prop
+        [(varp v) v]
+        [(notp p) (string-append "~(" (print-prop p) ")")]
+        [(andp p1 p2) (string-append "(" (print-prop p1) " ^ " (print-prop p2) ")")]
+        [(orp p1 p2) (string-append "(" (print-prop p1) " v " (print-prop p2) ")")]))
+
+(test (print-prop (varp "a")) "a")
+(test (print-prop (notp (varp "a"))) "~(a)")
+(test (print-prop (andp (varp "a") (varp "b"))) "(a ^ b)")
+(test (print-prop (orp (varp "a") (varp "b"))) "(a v b)")
+(test (print-prop (andp (varp "a") (orp (varp "a") (varp "b")))) "(a ^ (a v b))")
+(test (print-prop (orp (varp "a") (notp (varp "a")))) "(a v ~(a))")
+(test (print-prop (orp (andp (varp "a") (varp "b")) (orp (varp "a") (varp "b")))) "((a ^ b) v (a v b))")
+
 #| Parte B |#
 
 ;; occurrences :: Prop String -> Number
@@ -124,27 +141,97 @@
 ;; retorna #t si la proposición es una tautología, es decir, si es verdadera para cualquier ambiente de evaluación
 (define (tautology? prop)
     (let ([envs (all-environments (vars prop))])
-        (fold (lambda env all-true? (and all-true? (eval prop env))) #t envs)))
+        (foldl (lambda (env all_true) (and all_true (eval prop env))) #t envs)))
     
+(test (tautology? (varp "a")) #f)
+(test (tautology? (notp (varp "a"))) #f)
+(test (tautology? (andp (varp "a") (varp "b"))) #f)
+(test (tautology? (orp (varp "a") (varp "b"))) #f)
+(test (tautology? (andp (varp "a") (orp (varp "a") (varp "b")))) #f)
+(test (tautology? (orp (varp "a") (notp (varp "a")))) #t)
     
 #| P2 |#
 
 #| Parte A |#
 
 ;; simplify-negations :: Prop -> Prop
+;; simplifica las negaciones de una proposición, aplicando las siguientes reglas:
+;; ~(~p) = p ; ~(p ^ q) = ~p v ~q ; ~(p v q) = ~p ^ ~q
+(define (simplify-negations prop)
+    (match prop
+        [(varp p) prop]
+        [(notp (varp p)) prop]
+        [(andp p1 p2) (andp (simplify-negations p1) (simplify-negations p2))]
+        [(orp p1 p2) (orp (simplify-negations p1) (simplify-negations p2))]
+        [(notp (notp p)) simplify-negations p] ; ~~p = p
+        [(notp (andp p1 p2)) (orp (notp (simplify-negations p1)) (notp (simplify-negations p2)))] ; ~(p ^ q) = ~p v ~q
+        [(notp (orp p1 p2)) (andp (notp (simplify-negations p1)) (notp (simplify-negations p2)))] ; ~(p v q) = ~p ^ ~q
+    )
+)
+
+(test (simplify-negations (notp (notp (varp "a")))) (varp "a"))
+(test (simplify-negations (notp (andp (varp "a") (varp "b")))) (orp (notp (varp "a")) (notp (varp "b"))))
+(test (simplify-negations (notp (orp (varp "a") (varp "b")))) (andp (notp (varp "a")) (notp (varp "b"))))
+(test (simplify-negations (notp (notp (andp (varp "a") (varp "b"))))) (andp (varp "a") (varp "b")))
+(test (simplify-negations (notp (notp (orp (varp "a") (varp "b"))))) (orp (varp "a") (varp "b")))
+(test (simplify-negations (notp (orp (notp (varp "a")) (varp "b")))) (andp (notp (notp (varp "a"))) (notp (varp "b"))))
 
 #| Parte B |#
 
 ;; distribute-and :: Prop -> Prop
+;; distribuye los ands en una proposición, aplicando las siguientes reglas:
+;; p ^ (q v r) = (p ^ q) v (p ^ r) ; (p v q) ^ r = (p ^ r) v (q ^ r)
+(define (distribute-and prop)
+    (match prop
+        [(andp p1 (orp p2 p3)) (orp (distribute-and (andp p1 p2)) (distribute-and (andp p1 p3)))]
+        [(andp (orp p1 p2) p3) (orp (distribute-and (andp p1 p3)) (distribute-and (andp p2 p3)))]
+        [(andp p1 p2) (andp (distribute-and p1) (distribute-and p2))]
+        [(orp p1 p2) (orp (distribute-and p1) (distribute-and p2))]
+        [else prop]))
+
+(test (distribute-and (andp (varp "a") (orp (varp "b") (varp "c")))) (orp (andp (varp "a") (varp "b")) (andp (varp "a") (varp "c"))))
+(test (distribute-and (andp (orp (varp "a") (varp "b")) (varp "c"))) (orp (andp (varp "a") (varp "c")) (andp (varp "b") (varp "c"))))
+(test (distribute-and (andp (varp "a") (varp "b"))) (andp (varp "a") (varp "b")))
+(test (distribute-and (orp (varp "a") (varp "b"))) (orp (varp "a") (varp "b")))
+(test (distribute-and (notp (varp "a"))) (notp (varp "a")))
+(test (distribute-and (andp (notp (varp "a")) (orp (varp "b") (varp "c")))) (orp (andp (notp (varp "a")) (varp "b")) (andp (notp (varp "a")) (varp "c"))))
+
 
 #| Parte C |#
 
 ;; apply-until :: (a -> a) (a a -> Boolean) -> a -> a
+;; aplica una función f hasta que se cumpla una condición c donde c recibe el valor anterior y el nuevo valor
+;; devuelve la función que toma un valor inicial, aplica f hasta que se cumpla c y devuelve el último valor
+(define (apply-until func cond-func)
+    (lambda (initial-value)
+        (let ([x-new (func initial-value)])
+            (if (cond-func initial-value x-new)
+                x-new
+                ((apply-until func cond-func) x-new)))))
+
+(test ((apply-until (lambda (x) (+ x 1)) (lambda (x new-x) (> new-x 10))) 0) 11)
+(test ((apply-until (lambda (x) (/ x (add1 x))) (lambda (x new-x) (<= (- x new-x) 0.1))) 1) 0.25)
+(define (drop-one-item lst item)
+    (match lst
+        ['() '()]
+        [(cons head tail) (if 
+                            (equal? head item)
+                            tail
+                            (cons head (drop-one-item tail item)))]))
+(test ((apply-until (lambda (lst) (drop-one-item lst 1)) (lambda (lst new-lst) (equal? lst new-lst))) (list 1 1 2 3 1 4 1 5)) (list 2 3 4 5))
 
 #| Parte D |#
 
 ;; DNF :: Prop -> Prop
+;; devuelve la forma normal disyuntiva de una proposición
+(define (DNF prop)
+    (let ((simplify-all-negations (apply-until simplify-negations (lambda (p1 p2) (equal? p1 p2)))))
+        (let ((distribute-all-ands (apply-until distribute-and (lambda (p1 p2) (equal? p1 p2)))))
+            (distribute-all-ands (simplify-all-negations prop)))))
 
+(test (DNF (andp (varp "a") (orp (varp "b") (varp "c"))))
+    (orp (andp (varp "a") (varp "b")) (andp (varp "a") (varp "c"))))
+; (test (DNF (notp    
 
 
 #| P3 |#
