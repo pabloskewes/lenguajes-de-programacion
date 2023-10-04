581bffde886eca450a04899f33dfdc14f7b50e03
Pablo <112183476+pabloscopeo@users.noreply.github.com>
2023-10-04 01:06:35 -0300

    finished until p1.d

--- tarea2/T2.rkt
+++ tarea2/T2.rkt
@@ -13,7 +13,8 @@
         | (leq <expr> <expr>)
         | (ifc <expr> <expr> <expr>)
         | (id <sym>)
-
+        | (fun ListOf[<sym>] <expr>)
+        | (app <expr> ListOf[<expr>])
 |#
 (deftype Expr
   ;; core
@@ -27,15 +28,27 @@
   (leq l r)
   ;; condicion
   (ifc c t e)
-  ; ;; identificador
-  ; (id x)
-  ; ;; funcion con múltiples parámetros
-  ; (fun (id ...) e)
-  ; ;; aplicación con múltiples argumentos
-  ; (app f (id ...)))
+  ;; identificador
+  (id x)
+  ;; funcion con múltiples parámetros
+  (fun params body)
+  ;; aplicación con múltiples argumentos
+  (app f-name args))
   
 
-(test Expr? (num 1) #t)
+(test (Expr? (num 1)) #t)
+(test (Expr? (add (num 1) (num 2))) #t)
+(test (Expr? (sub (num 1) (num 2))) #t)
+(test (Expr? (mul (num 1) (num 2))) #t)
+(test (Expr? (tt)) #t)
+(test (Expr? (ff)) #t)
+(test (Expr? (leq (num 1) (num 2))) #t)
+(test (Expr? (ifc (num 1) (num 2) (num 3))) #t)
+(test (Expr? (id 'x)) #t)
+(test (Expr? (fun (list 'x 'y) (add (id 'x) (id 'y)))) #t)
+(test (Expr? (app (id 'my-function) (list (num 1) (num 2) (num 3)))) #t)
+(test (Expr? (fun (list 'x 'y 'z) (mul (sub (id 'x) (id 'y)) (id 'z)))) #t)
+
 
 ;; concrete syntax
 #|
@@ -47,6 +60,7 @@
             | 'false
             | (list '<= <s-expr> <s-expr>)
             | (list 'if <s-expr> <s-expr> <s-expr>)
+
 |#
 
 ;; parse :: s-expr -> Expr
@@ -62,8 +76,13 @@
     [(list '<= l r) (leq (parse l) (parse r))]
     [(list 'if c t e) (ifc (parse c) (parse t) (parse e))]
     ; se deja el error por mientras por si acaso
+    [x #:when (symbol? x) (id x)]
+    [(list 'fun (list params ...) body) (fun params (parse body))]
+    [(list f-name args ...) (app (parse f-name) (map parse args))]
+
     [else (error 'parse "invalid s-expr: ~a" s-expr)]))
   
+; tests p1.a
 (test (parse 1) (num 1))
 (test (parse (list '+ 1 2)) (add (num 1) (num 2)))
 (test (parse '(+ 7 9)) (add (num 7) (num 9)))
@@ -81,13 +100,19 @@
 (test (parse '(<= 3 5)) (leq (num 3) (num 5)))
 (test (parse '(if (<= 3 5) 2 4)) (ifc (leq (num 3) (num 5)) (num 2) (num 4)))
 (test (parse '(if true 2 4)) (ifc (tt) (num 2) (num 4)))
+; tests p1.b
+(test (parse 'x) (id 'x))
+(test (parse '(fun (x y) (+ x y))) (fun (list 'x 'y) (add (id 'x) (id 'y))))
+(test (parse '(my-function 2 3 4)) (app (id 'my-function) (list (num 2) (num 3) (num 4)))) 
 
 
 ;; PARTE 1C, 1G
 
 (deftype Val
-  ;...
-  )
+  (numV n)
+  (boolV b)
+  (closureV id body env))
+
 
 ;; ambiente de sustitución diferida
 (deftype Env
@@ -111,16 +136,48 @@
 
 ;; PARTE 1D
 
-;; num2num-op :: ...
-(define (num2num-op) '???)
+;; num2num-op :: (Number Number -> Number)-> (Val Val -> Val)
+; Recibe una función que realiza una operación binaria sobre dos números (retornan-
+; do un número) y retorna una función que recibe dos valores, hace pattern
+; matching sobre los constructores para acceder a los números, y aplica la ope-
+; ración, retornando un nuevo valor. Si los dos valores que recibe no son de
+; carácter numérico, la función debe fallar.
+(define (num2num-op op)
+  (lambda (v1 v2)
+    (match (list v1 v2)
+      [(list (numV n1) (numV n2)) (numV (op n1 n2))]
+      [else (error 'num2num-op "invalid operands, expected numbers, got: ~a" (list v1 v2))])))
 
-;; num2bool-op :: ...
-(define (num2bool-op) '???)
+
+;; num2bool-op :: (Number Number -> Boolean)-> (Val Val -> Val)
+; Análoga a num2num-op, pero retorna un valor de carácter booleano.
+(define (num2bool-op op)
+  (lambda (v1 v2)
+    (match (list v1 v2)
+      [(list (numV n1) (numV n2)) (boolV (op n1 n2))]
+      [else (error 'num2bool-op "invalid operands, expected numbers, got: ~a" (list v1 v2))])))
+
+
+(define num+ (num2num-op +))
+(define num- (num2num-op -))
+(define num* (num2num-op *))
+(define num<= (num2bool-op <=))
+
+(test (num+ (numV 1) (numV 2)) (numV 3))
+(test (num- (numV 1) (numV 2)) (numV -1))
+(test (num* (numV 1) (numV 2)) (numV 2))
+(test (num<= (numV 1) (numV 2)) (boolV #t))
+(test (num<= (numV 2) (numV 1)) (boolV #f))
+(test/exn (num+ (numV 1) (boolV #t)) "invalid operands")
 
 ;; PARTE 1E, 1G
 
-;; eval :: ...
-(define (eval) '???)
+;; eval :: Expr Env -> Val
+;; Evalúa una expresión en un ambiente dado.
+(define (eval expr env)
+  (match expr
+    [(num n) n]
+
 
 ;; PARTE 2A
 
